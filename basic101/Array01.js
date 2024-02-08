0e4280112f4ad60c4a836889699f2674f9dfcf9f
KushalDas-KD <124072345+KushalDas-KD@users.noreply.github.com>
2024-02-08 15:05:28 +0000

    Configer devcontainer

---
+++ basic101/Array01.js
@@ -0,0 +1,5 @@
+const myArr= new Array(1,2,3,4);
+console.log(myArr[1]);
+myArr.push(6);
+myArr.pop();
+console.log(myArr);
---
+++ basic101/date_time.js
@@ -0,0 +1,35 @@
+let date = new Date()
+console.log("toDateString  "+date.toDateString());
+
+console.log("******************");
+
+console.log("toISOString  "+date.toISOString());
+
+console.log("******************");
+
+console.log("toJSON  "+date.toJSON());
+
+console.log("******************");
+
+console.log("toLocaleDateString  "+date.toLocaleDateString());
+
+console.log("******************");
+
+console.log("toLocaleString  "+date.toLocaleString());
+
+console.log("******************");
+
+console.log("toLocaleTimeString  "+date.toLocaleTimeString());
+
+console.log("******************");
+
+console.log("toString  "+date.toString());
+
+console.log("******************");
+
+console.log("toTimeString  "+date.toTimeString());
+
+console.log("******************");
+
+console.log("toUTCString  "+date.toUTCString());
+
